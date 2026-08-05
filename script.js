613089ff13f1c6a8f7a62cc193fb9d484468e973
binarybrain-debug <ydvsameer10o@gmail.com>
2026-08-05 12:38:26 +0530

    Implement login functionality with validation

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Binary Brain Assessment Portal Loaded");
+const loginPage = document.getElementById("loginPage");
+const testPage = document.getElementById("testPage");
+
+const startBtn = document.getElementById("startBtn");
+
+const studentName = document.getElementById("studentName");
+
+startBtn.addEventListener("click",()=>{
+
+let name=document.getElementById("name").value;
+
+let pass=document.getElementById("password").value;
+
+if(name=="" || pass==""){
+alert("Please enter Name and Password");
+return;
+}
+
+loginPage.style.display="none";
+testPage.style.display="block";
+
+studentName.innerHTML="Welcome, "+name;
+
 });
